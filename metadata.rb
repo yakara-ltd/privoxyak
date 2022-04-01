227d90adc43ce35e046c6f52a3b3819604c626a7
Nathan James <wobaekat@gmail.com>
2022-04-01 13:05:25 +0100

    chef13 fixes and repolist fix

--- metadata.rb
+++ metadata.rb
@@ -1,11 +1,10 @@
 # frozen_string_literal: true
 
 name             'privoxyak'
-maintainer       'James Le Cuirot'
-maintainer_email 'james.le-cuirot@yakara.com'
+maintainer       'Nathan James'
+maintainer_email 'nathan.james@yakara.com'
 license          'Apache-2.0'
 description      'Installs and configures Privoxy'
-long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.1.0'
 chef_version     '>= 13.0'
 
